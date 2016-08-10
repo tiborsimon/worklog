31a70177a2625b672d0f2c3379ed644420f09233
Tibor Simon <tibor@tiborsimon.io>
2016-08-10 16:44:08 +0200

    New deployment method added.

--- scripts/deploy_to_gh_pages.sh
+++ scripts/deploy_to_gh_pages.sh
@@ -2,18 +2,19 @@
 # See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
 set -o errexit
 
-mkdir publish
-
 # config
 git config --global user.email "nobody@nobody.org"
 git config --global user.name "Travis CI"
+
+mkdir publish
+cd publish
+git clone https://github.com/tiborsimon/worklog.git
+git checkout gh-pages
+
 
 # build
 make publish
 
 # deploy
-cd publish
-git init
-git add worklog.json
-git commit -m "Deploy worklog."
+git commit -am "Deploy worklog."
 git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
